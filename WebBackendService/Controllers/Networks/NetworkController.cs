ae03add4e7abca06d5a4a566ad6b1a6a9ed86816
jinwei69 <ss610349@gmail.com>
2022-06-27 09:09:03 +0800

    Add NetworkController.cs

---
+++ WebBackendService/Controllers/Networks/NetworkController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebBackendService.Controllers.Networks
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetworkController : ControllerBase
+    {
+    }
+}
